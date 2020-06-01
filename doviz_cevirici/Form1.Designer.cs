eeaef0cd4dc00fbdee95c5b985a28d9d512cf1e5
Onur ÖZEL <sumotoma_onur@hotmail.com>
2020-06-02 00:02:07 +0300

    Add files via upload

---
+++ doviz_cevirici/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace doviz_cevirici
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.amountTextBox = new System.Windows.Forms.TextBox();
+            this.fromComboBox = new System.Windows.Forms.ComboBox();
+            this.toComboBox = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.amountLabel = new System.Windows.Forms.Label();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // amountTextBox
+            // 
+            this.amountTextBox.Location = new System.Drawing.Point(16, 30);
+            this.amountTextBox.Name = "amountTextBox";
+            this.amountTextBox.Size = new System.Drawing.Size(100, 20);
+            this.amountTextBox.TabIndex = 0;
+            // 
+            // fromComboBox
+            // 
+            this.fromComboBox.FormattingEnabled = true;
+            this.fromComboBox.Items.AddRange(new object[] {
+            "Choose...",
+            "TRY",
+            "USD",
+            "AUD",
+            "DKK",
+            "EUR",
+            "GBP",
+            "CHF",
+            "SEK",
+            "CAD",
+            "KWD",
+            "NOK",
+            "SAR",
+            "JPY"});
+            this.fromComboBox.Location = new System.Drawing.Point(122, 30);
+            this.fromComboBox.Name = "fromComboBox";
+            this.fromComboBox.Size = new System.Drawing.Size(121, 21);
+            this.fromComboBox.TabIndex = 1;
+            // 
+            // toComboBox
+            // 
+            this.toComboBox.FormattingEnabled = true;
+            this.toComboBox.Items.AddRange(new object[] {
+            "Choose...",
+            "TRY",
+            "USD",
+            "AUD",
+            "DKK",
+            "EUR",
+            "GBP",
+            "CHF",
+            "SEK",
+            "CAD",
+            "KWD",
+            "NOK",
+            "SAR",
+            "JPY"});
+            this.toComboBox.Location = new System.Drawing.Point(249, 30);
+            this.toComboBox.Name = "toComboBox";
+            this.toComboBox.Size = new System.Drawing.Size(121, 21);
+            this.toComboBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Amount";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(119, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 16);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "From";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(246, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 16);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "To";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(16, 130);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(354, 160);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "EXCHANGE";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // amountLabel
+            // 
+            this.amountLabel.AutoSize = true;
+            this.amountLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.amountLabel.Location = new System.Drawing.Point(145, 86);
+            this.amountLabel.Name = "amountLabel";
+            this.amountLabel.Size = new System.Drawing.Size(0, 31);
+            this.amountLabel.TabIndex = 9;
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resultLabel.Location = new System.Drawing.Point(13, 86);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(0, 24);
+            this.resultLabel.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 316);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.amountLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toComboBox);
+            this.Controls.Add(this.fromComboBox);
+            this.Controls.Add(this.amountTextBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Currency Exchanger v1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox amountTextBox;
+        private System.Windows.Forms.ComboBox fromComboBox;
+        private System.Windows.Forms.ComboBox toComboBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label amountLabel;
+        private System.Windows.Forms.Label resultLabel;
+    }
+}
+
---
+++ doviz_cevirici/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace doviz_cevirici
+{
+    public partial class Form1 : Form
+    {
+        double USD, AUD, DKK, EUR, GBP, CHF, SEK, CAD, KWD, NOK, SAR, JPY;
+        double amountTRY;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string from = fromComboBox.SelectedItem.ToString();
+            string to = toComboBox.SelectedItem.ToString();
+            double amount = Convert.ToDouble(amountTextBox.Text);
+
+            Exchange(from,amount);
+
+            
+            switch (to)
+            {
+                case "TRY":
+                    resultLabel.Text = amount + " " + from + " : "+amountTRY.ToString() + " TRY";
+                    break;
+                case "USD":
+                    resultLabel.Text = amount +" "+from+" : "+(amountTRY / USD).ToString()+" USD";
+                    break;
+                case "AUD":
+                    resultLabel.Text = amount + " " + from + " : " + (amountTRY / AUD).ToString() + " AUD";
+                    break;
+                case "DKK":
+                    resultLabel.Text = amount + " " + from + " : " + (amountTRY / DKK).ToString() + " DKK";
+                    break;
+                case "EUR":
+                    resultLabel.Text = amount + " " + from + " : " + (amountTRY / EUR).ToString() + " EUR";
+                    break;
+                case "GBP":
+                    resultLabel.Text = amount + " " + from + " : " + (amountTRY / GBP).ToString() + " GBP";
+                    break;
+                case "CHF":
+                    resultLabel.Text = amount + " " + from + " : " + (amountTRY / CHF).ToString() + " CHF";
+                    break;
+                case "SEK":
+                    resultLabel.Text = amount + " " + from + " : " + (amountTRY / SEK).ToString() + " SEK";
+                    break;
+                case "CAD":
+                    resultLabel.Text = amount + " " + from + " : " + (amountTRY / CAD).ToString() + " CAD";
+                    break;
+                case "KWD":
+                    resultLabel.Text = amount + " " + from + " : " + (amountTRY / KWD).ToString() + " KWD";
+                    break;
+                case "NOK":
+                    resultLabel.Text = amount + " " + from + " : " + (amountTRY / NOK).ToString() + " NOK";
+                    break;
+                case "SAR":
+                    resultLabel.Text = amount + " " + from + " : " + (amountTRY / SAR).ToString() + " SAR";
+                    break;
+                case "JPY":
+                    resultLabel.Text = amount + " " + from + " : " + (amountTRY / JPY).ToString() + " JPY";
+                    break;
+        
+            }
+
+        
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            fromComboBox.SelectedIndex = 0;
+            toComboBox.SelectedIndex = 0;
+            string todayXML = "https://www.tcmb.gov.tr/kurlar/today.xml";
+            var xmldocument = new XmlDocument();
+            xmldocument.Load(todayXML);            
+            USD = Convert.ToDouble(xmldocument.SelectSingleNode("Tarih_Date/Currency[@Kod = 'USD']/BanknoteSelling").InnerXml);
+            AUD = Convert.ToDouble(xmldocument.SelectSingleNode("Tarih_Date/Currency[@Kod = 'AUD']/BanknoteSelling").InnerXml);
+            DKK = Convert.ToDouble(xmldocument.SelectSingleNode("Tarih_Date/Currency[@Kod = 'DKK']/BanknoteSelling").InnerXml);
+            EUR = Convert.ToDouble(xmldocument.SelectSingleNode("Tarih_Date/Currency[@Kod = 'EUR']/BanknoteSelling").InnerXml);
+            GBP = Convert.ToDouble(xmldocument.SelectSingleNode("Tarih_Date/Currency[@Kod = 'GBP']/BanknoteSelling").InnerXml);
+            CHF = Convert.ToDouble(xmldocument.SelectSingleNode("Tarih_Date/Currency[@Kod = 'CHF']/BanknoteSelling").InnerXml);
+            SEK = Convert.ToDouble(xmldocument.SelectSingleNode("Tarih_Date/Currency[@Kod = 'SEK']/BanknoteSelling").InnerXml);
+            CAD = Convert.ToDouble(xmldocument.SelectSingleNode("Tarih_Date/Currency[@Kod = 'CAD']/BanknoteSelling").InnerXml);
+            KWD = Convert.ToDouble(xmldocument.SelectSingleNode("Tarih_Date/Currency[@Kod = 'KWD']/BanknoteSelling").InnerXml);
+            NOK = Convert.ToDouble(xmldocument.SelectSingleNode("Tarih_Date/Currency[@Kod = 'NOK']/BanknoteSelling").InnerXml);
+            SAR = Convert.ToDouble(xmldocument.SelectSingleNode("Tarih_Date/Currency[@Kod = 'SAR']/BanknoteSelling").InnerXml);
+            JPY = Convert.ToDouble(xmldocument.SelectSingleNode("Tarih_Date/Currency[@Kod = 'JPY']/BanknoteSelling").InnerXml);
+
+
+         
+        }
+
+        private void Exchange(string from,double amount)
+        {
+            
+            switch (from)
+            {
+                case "TRY":
+                    amountTRY = amount;
+                    break;
+                case "USD":
+                    amountTRY = amount * USD;
+                    break;
+                case "AUD":
+                    amountTRY = (amount * AUD);
+                    break;
+                case "DKK":
+                    amountTRY = (amount * DKK);
+                    break;
+                case "EUR":
+                    amountTRY = (amount * EUR);
+                    break;
+                case "GBP":
+                    amountTRY = (amount * GBP);
+                    break;
+                case "CHF":
+                    amountTRY = (amount * CHF);
+                    break;
+                case "SEK":
+                    amountTRY = (amount * SEK);
+                    break;
+                case "CAD":
+                    amountTRY = (amount * CAD);
+                    break;
+                case "KWD":
+                    amountTRY = (amount * KWD);
+                    break;
+                case "NOK":
+                    amountTRY = (amount * NOK);
+                    break;
+                case "SAR":
+                    amountTRY = (amount * SAR);
+                    break;
+                case "JPY":
+                    amountTRY = (amount * JPY);
+                    break;
+            }
+        }
+    }
+}
